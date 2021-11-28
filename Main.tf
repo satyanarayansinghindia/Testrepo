0723b54c8f7ff36cef8ec006fc45b0dbc3a3f12c
satyanarayansinghindia <61449509+satyanarayansinghindia@users.noreply.github.com>
2021-11-28 19:13:30 +0530

    Update Main.tf

--- Main.tf
+++ Main.tf
@@ -1,1 +1,25 @@
+provider "google" {
+project = "skillful-air-333110"
+region = "europe-north1"
+credentials = "svckey01.json"
+}
 
+variable "machine" {
+type = string
+default = "e2-standard-2"
+}
+
+resource "google_compute_instance" "newvm1" {
+name = "appvm"
+machine_type = var.machine
+zone = "europe-north1"
+network_interface {
+network = "vpc001"
+subnet = "subnet2"
+}
+boot_disk {
+initialize_params {
+image = "windows-server-2012-r2-dc-core-v20200609"
+}
+}
+}
